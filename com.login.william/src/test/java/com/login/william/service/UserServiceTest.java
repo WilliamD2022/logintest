747d75c8ed68abc08eddad7c26c6e5e27d0b57de
William Domingues <williamcdzx88@gmail.com>
2025-10-25 19:22:43 -0300

    ci: adiciona job unit-tests antes de api-tests e agrega Allure

--- com.login.william/src/test/java/com/login/william/service/UserServiceTest.java
+++ com.login.william/src/test/java/com/login/william/service/UserServiceTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class UserServiceTest {
-
+    @Tag("unit")
     @Tag("api")
     @Test
     void deveCriarUsuarioComSenhaCriptografada() {
