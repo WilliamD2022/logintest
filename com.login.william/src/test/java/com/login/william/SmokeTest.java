6ddd395cb74049bcf62a3517d2bcedb1bf33111d
William Domingues <williamcdzx88@gmail.com>
2025-10-25 18:57:27 -0300

    ci: corrigir paths, gerar Allure e empacotar JAR (deploy-prod simulado)

--- com.login.william/src/test/java/com/login/william/SmokeTest.java
+++ com.login.william/src/test/java/com/login/william/SmokeTest.java
@@ -1,22 +1,11 @@
 package com.login.william;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.junit5.AllureJunit5;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Epic("Auth")
-@Feature("Smoke")
-@ExtendWith(AllureJunit5.class)
 class SmokeTest {
-
     @Test
-    @Description("Smoke: valida execução do pipeline e geração do Allure.")
-    void ok() {
+    void shouldPass() {
         assertTrue(true);
     }
 }
